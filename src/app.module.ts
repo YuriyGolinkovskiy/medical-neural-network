cb7dde04ce36480f317f5826ffbaeabf9128e7de
Yuriy Golinkovskiy <yuragolinkovskiy@gmail.com>
2022-05-19 15:34:45 +0300

    add dataset part_1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { NetworkModule } from './network/network.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UsersModule } from './users/users.module';
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     ),
     UsersModule,
+    NetworkModule,
   ],
   controllers: [],
   providers: [],
